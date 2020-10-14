c69d1a0401b7f93ea6380fa7ef40cc2b9e8b72b9
wndks <wndkswndks222@gmail.com>
2020-10-14 17:25:11 +0900

    새커2커밋2

--- MyStyle/Src/main.c
+++ MyStyle/Src/main.c
@@ -78,7 +78,7 @@ static void MX_TIM10_Init(void);
   * @brief  The application entry point.
   * @retval int
   */
-int main(void)//==//111
+int main(void)//==//111222
 {
   /* USER CODE BEGIN 1 */
 	static uint8_t databuff[5]={0,}; 
